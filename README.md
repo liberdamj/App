54d8d6e8bf8deefd2fb8a5d0b4df6d188ebd3952
liberdamj <liberdamj@gmail.com>
2019-07-28 08:01:42 -0600

    Added my.kv file and thus re-vamped exampleApp.py using the design language of kivy in my.kv. Edited README.md as well to accomidate for these changes and explain the .kv file.

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ Personal App Design
 This Repo contains Apps Designed by Malachi Liberda with the python module Kivy.
 
 The file exampleApp.py is for testing purposes and as an example app currently, for reference when programming other Apps.
+
+---exampleApp.py---
+Explanation of File System:
+
+All filed - exampleApp.py && my.kv
+
+my.kv Explanation -
+my.kv is for the styling of the app. The reason it is named my.kv is related to the class in the main file(exampleApp.py). The main class is exampleApp.py is MyApp. Therefore, we remove the "App" aspect of that string and we name the .kv file the preceding string in all lowercase.
+
+Alternate Example: Class is named HelloApp, we name the .kv file hello.kv
+Second Example: Class is named BruceSpringsteen, we name the .kv file brucespringsteen.kv since we have no "App" string to remove, we just lowercase the entire string.
+
+To clarify, when main class is run, it looks for a file called "classname.kv". So in our case, when we run exampleApp.py, and it calls class MyApp, kivy looks for a file called my.kv and runs it.
--- exampleApp.py
+++ exampleApp.py
@@ -4,44 +4,11 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
 #Grid Layout Class
-class MyGrid(GridLayout):
-    def __init__(self, **kwargs):
-
-        super(MyGrid, self).__init__(**kwargs)
-        self.cols = 1
-
-        self.inside = GridLayout()
-        self.inside.cols = 2
-
-        self.inside.add_widget(Label(text="First Name: "))
-        self.name = TextInput(multiline=False)
-        self.inside.add_widget(self.name)
-
-        self.inside.add_widget(Label(text="Last Name: "))
-        self.lastName = TextInput(multiline=False)
-        self.inside.add_widget(self.lastName)
-
-        self.inside.add_widget(Label(text="E-mail: "))
-        self.email = TextInput(multiline=False)
-        self.inside.add_widget(self.email)
-
-        self.add_widget(self.inside)
-
-        self.submit = Button(text="Submit", font_size=36)
-        self.submit.bind(on_press=self.pressed)
-        self.add_widget(self.submit)
-
-    #Button Function to do _ when Button submit is pressed.
-    def pressed(self, instance):
-        name = self.name.text
-        lastName = self.lastName.text
-        email = self.email.text
-        print("Name: ", name, " Last Name: ", lastName, " E-mail: ", email)
-        self.name.text = ""
-        self.lastName.text = ""
-        self.email.text = ""
+class MyGrid(Widget):
+    pass
 
 #Building Class
 class MyApp(App):
