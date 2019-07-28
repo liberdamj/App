991a4b687b18d0387cbe1258568d462e9367d2a2
liberdamj <liberdamj@gmail.com>
2019-07-28 09:05:38 -0600

    Added multi-window capability to app. Also includes 'password' integration.

--- exampleApp.py
+++ exampleApp.py
@@ -10,32 +10,27 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Rectangle
 from kivy.graphics import Color
 from kivy.graphics import Line
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-#Touch Class
-class Touch(Widget):
+class MainWindow(Screen):
+    pass
 
-    def __init__(self, **kwargs):
-        super(Touch, self).__init__(**kwargs)
+class SecondWindow(Screen):
+    pass
 
-        with self.canvas:
-            Line(points=(20, 30, 400, 500, 60, 500))
-            Color(1, 0, 0, .5, mode='rgba')
-            self.rect = Rectangle(pos=(0, 0), size=(50, 50))
+class WindowManager(ScreenManager):
+    pass
 
-    def on_touch_down(self, touch):
-        self.rect.pos = touch.pos
-        print("Mouse Down", touch)
 
-    def on_touch_move(self, touch):
-        self.rect.pos = touch.pos
-        print("Mouse Move", touch)
+kv = Builder.load_file("my.kv")
 
 
 #Building Class
-class MyApp(App):
+class MyMainApp(App):
     def build(self):
-        return Touch()
+        return kv
 
 #Runs MyApp Method
 if __name__ == "__main__":
-    MyApp().run()
+    MyMainApp().run()
