249668e89956663357407a5d294d0b2ceacf2058
liberdamj <liberdamj@gmail.com>
2019-07-28 07:42:03 -0600

    Change to exampleApp.py: Buttons and button function defined and explained.

--- exampleApp.py
+++ exampleApp.py
@@ -3,28 +3,51 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
+#Grid Layout Class
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
 
         super(MyGrid, self).__init__(**kwargs)
+        self.cols = 1
 
-        self.cols = 4
-        self.add_widget(Label(text="First Name: "))
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+        self.inside.add_widget(Label(text="First Name: "))
         self.name = TextInput(multiline=False)
-        self.add_widget(self.name)
+        self.inside.add_widget(self.name)
 
-        self.add_widget(Label(text="Last Name: "))
+        self.inside.add_widget(Label(text="Last Name: "))
         self.lastName = TextInput(multiline=False)
-        self.add_widget(self.lastName)
+        self.inside.add_widget(self.lastName)
 
-        self.add_widget(Label(text="E-mail: "))
+        self.inside.add_widget(Label(text="E-mail: "))
         self.email = TextInput(multiline=False)
-        self.add_widget(self.email)
+        self.inside.add_widget(self.email)
 
+        self.add_widget(self.inside)
+
+        self.submit = Button(text="Submit", font_size=36)
+        self.submit.bind(on_press=self.pressed)
+        self.add_widget(self.submit)
+
+    #Button Function to do _ when Button submit is pressed.
+    def pressed(self, instance):
+        name = self.name.text
+        lastName = self.lastName.text
+        email = self.email.text
+        print("Name: ", name, " Last Name: ", lastName, " E-mail: ", email)
+        self.name.text = ""
+        self.lastName.text = ""
+        self.email.text = ""
+
+#Building Class
 class MyApp(App):
     def build(self):
         return MyGrid()
 
+#Runs MyApp Method
 if __name__ == "__main__":
     MyApp().run()
