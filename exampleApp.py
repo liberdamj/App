e47af5c2c73a7f11da73e60a1aace943b308ca2a
liberdamj <liberdamj@gmail.com>
2019-07-28 08:37:51 -0600

    Float Layout and Button Functions (touch) are implemented.

--- exampleApp.py
+++ exampleApp.py
@@ -6,23 +6,28 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.uix.floatlayout import FloatLayout
 
-#Grid Layout Class
-class MyGrid(Widget):
-    name = ObjectProperty(None) #Initialized as None to start
-    lastName = ObjectProperty(None)
-    email = ObjectProperty(None)
+#Touch Class
+class Touch(Widget):
 
-    def btn(self):
-        print("Name:", self.name.text, "Last Name:", self.lastName.text, "email:", self.email.text)
-        self.name.text = ""
-        self.lastName.text = ""
-        self.email.text = ""
+    btn = ObjectProperty(None)
+
+    def on_touch_down(self, touch):
+        print("Mouse Down", touch)
+        self.btn.opacity = 0.5
+
+    def on_touch_move(self, touch):
+        print("Mouse Move", touch)
+
+    def on_touch_up(self, touch):
+        print("Mouse Up", touch)
+        self.btn.opacity = 1
 
 #Building Class
 class MyApp(App):
     def build(self):
-        return MyGrid()
+        return Touch()
 
 #Runs MyApp Method
 if __name__ == "__main__":
