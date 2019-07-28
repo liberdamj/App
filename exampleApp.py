2be7c4536ae64a43878bdd03860acca6d3d5df4f
liberdamj <liberdamj@gmail.com>
2019-07-28 08:48:01 -0600

    Implemented Drawing in Kivy. Canvas and whatnot.

--- exampleApp.py
+++ exampleApp.py
@@ -7,22 +7,29 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Rectangle
+from kivy.graphics import Color
+from kivy.graphics import Line
 
 #Touch Class
 class Touch(Widget):
 
-    btn = ObjectProperty(None)
+    def __init__(self, **kwargs):
+        super(Touch, self).__init__(**kwargs)
+
+        with self.canvas:
+            Line(points=(20, 30, 400, 500, 60, 500))
+            Color(1, 0, 0, .5, mode='rgba')
+            self.rect = Rectangle(pos=(0, 0), size=(50, 50))
 
     def on_touch_down(self, touch):
+        self.rect.pos = touch.pos
         print("Mouse Down", touch)
-        self.btn.opacity = 0.5
 
     def on_touch_move(self, touch):
+        self.rect.pos = touch.pos
         print("Mouse Move", touch)
 
-    def on_touch_up(self, touch):
-        print("Mouse Up", touch)
-        self.btn.opacity = 1
 
 #Building Class
 class MyApp(App):
